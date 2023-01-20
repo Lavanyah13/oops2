e1b9e0303715fe6e8fe56328c5924f38a7c4125d
Lavanyah B <104270776+Lavanyah13@users.noreply.github.com>
2023-01-20 23:09:31 +0530

    Create predict_output.java

---
+++ predict_output.java
@@ -0,0 +1,16 @@
+class Student{
+    int roll_number;
+    String name;
+}
+class DPS {
+    public static void main (String[] args) {
+        Student s=new Student();
+        s.roll_number=5;
+        s.name="Rohit";
+        System.out.println(s.roll_number+" "+s.name);
+    }
+}
+
+
+
+ans: 5 Rohit
